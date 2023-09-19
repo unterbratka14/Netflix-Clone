1fe5748156e6dbc5490abb8bd48af1d2e358c106
Almas Ramazanov <ramazanov_almas@list.ru>
2023-09-19 17:27:01 +0600

    Modified gitignore with keys

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
-import { AuthContextProvider } from "./components/context/AUTHcontext";
 
 function App() {
   return (
     <>
-      <AuthContextProvider>
-        <NavBar />{" "}
-        {/* I want my NavBar to be displayed on all the pages, that is why I am not including it inside <Routes> */}
-        <Routes>
-          {/* This line of code gives me an opportunity to set the routes if we want to NOT re-render the page everytime I go to another React component. I include <Route> for every such React component, and it renders specific component according to the path given */}
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </AuthContextProvider>
+      <NavBar />{" "}
+      {/* I want my NavBar to be displayed on all the pages, that is why I am not including it inside <Routes> */}
+      <Routes>
+        {/* This line of code gives me an opportunity to set the routes if we want to NOT re-render the page everytime I go to another React component. I include <Route> for every such React component, and it renders specific component according to the path given */}
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
